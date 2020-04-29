ac868cc84214422746111a4295e6ff7c133c5c52
yagamiraku <yagami.rakuraku@gmail.com>
2020-04-29 16:39:58 +0800

    fix startLocation and update new level

--- danmakuGo/web-mobile/main.5daea.js
+++ danmakuGo/web-mobile/main.5daea.js
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.9c30d.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.6a0b0.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
@@ -178,7 +178,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.2fd38.js');
+        require('src/settings.d6aa6.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -186,7 +186,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.2fd38.js');
+        require('src/settings.d6aa6.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
