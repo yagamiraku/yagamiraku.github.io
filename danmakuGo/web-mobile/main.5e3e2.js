5356979dea0aa57fd70a3c775e81ef0b3983a5c9
yagamiraku <yagami.rakuraku@gmail.com>
2020-04-30 10:18:03 +0800

    remove BGM

--- danmakuGo/web-mobile/main.5e3e2.js
+++ danmakuGo/web-mobile/main.5e3e2.js
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.f1ee9.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.d3cd8.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
@@ -178,7 +178,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.d6aa6.js');
+        require('src/settings.1b1e1.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -186,7 +186,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.d6aa6.js');
+        require('src/settings.1b1e1.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
