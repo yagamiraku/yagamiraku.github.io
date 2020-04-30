ee7abbae62370e1bd2414594dc1a7d8303777c1b
yagamiraku <yagami.rakuraku@gmail.com>
2020-04-30 22:17:42 +0800

    update

--- carRun/web-mobile/main.6216b.js
+++ carRun/web-mobile/main.6216b.js
@@ -178,7 +178,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.3b1cb.js');
+        require('src/settings.c45dd.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -186,7 +186,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.3b1cb.js');
+        require('src/settings.c45dd.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
