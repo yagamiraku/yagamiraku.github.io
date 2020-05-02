674c2e2bfbaf599a625c49195a28ebb6be9033dc
yagamiraku <yagami.rakuraku@gmail.com>
2020-05-02 13:55:08 +0800

    fix enemy move AI with creater

--- carRun/web-mobile/main.39a23.js
+++ carRun/web-mobile/main.39a23.js
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.569e0.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.bd5db.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
@@ -178,7 +178,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.c5359.js');
+        require('src/settings.019e9.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -186,7 +186,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.c5359.js');
+        require('src/settings.019e9.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
