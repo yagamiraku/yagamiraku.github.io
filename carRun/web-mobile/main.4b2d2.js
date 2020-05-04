3e22552c6701818712f3eaba464cec619b9b1e38
yagamiraku <yagami.rakuraku@gmail.com>
2020-05-04 19:19:09 +0800

    fix speed

--- carRun/web-mobile/main.4b2d2.js
+++ carRun/web-mobile/main.4b2d2.js
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.15d00.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.8f3f3.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
@@ -178,7 +178,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.abddd.js');
+        require('src/settings.7edbc.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -186,7 +186,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.abddd.js');
+        require('src/settings.7edbc.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
