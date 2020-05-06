57d4a4594ab4e1ff103bf722b25a0182827f201d
yagamiraku <yagami.rakuraku@gmail.com>
2020-05-06 14:51:49 +0800

    update

--- carRun/web-mobile/main.54df4.js
+++ carRun/web-mobile/main.54df4.js
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.07dce.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.13996.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
@@ -178,7 +178,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.85708.js');
+        require('src/settings.5e2d5.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -186,7 +186,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.85708.js');
+        require('src/settings.5e2d5.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
