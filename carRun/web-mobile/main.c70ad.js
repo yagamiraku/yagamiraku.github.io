d76020a07833fd732e444f1f8247888f9f2a99a9
yagamiraku <yagami.rakuraku@gmail.com>
2020-05-06 17:05:33 +0800

    bug fix

--- carRun/web-mobile/main.c70ad.js
+++ carRun/web-mobile/main.c70ad.js
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.13996.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.93cac.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
