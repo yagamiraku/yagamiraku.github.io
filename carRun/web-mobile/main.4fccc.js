3b1957d670af37d7d1d95bc5adf4e52701db68d2
yagamiraku <yagami.rakuraku@gmail.com>
2020-05-07 16:06:05 +0800

    update

--- carRun/web-mobile/main.4fccc.js
+++ carRun/web-mobile/main.4fccc.js
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.ee374.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.fc53c.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
