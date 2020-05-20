2398b652acf3614cbfe855d3abbec7a2ff458c58
yagamiraku <yagami.rakuraku@gmail.com>
2020-05-20 18:55:49 +0800

    update assets

--- FindPassword/web-mobile/main.bb316.js
+++ FindPassword/web-mobile/main.bb316.js
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.8d088.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.0cb63.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
@@ -178,7 +178,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.86b0e.js');
+        require('src/settings.cdd3a.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -186,7 +186,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.86b0e.js');
+        require('src/settings.cdd3a.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
